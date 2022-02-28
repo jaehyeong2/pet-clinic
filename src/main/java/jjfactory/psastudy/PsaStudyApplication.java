cf4239e57956c027bab76dcaaa45f6b7aa739521
jae <wogud1514@naver.com>
2022-02-28 15:41:41 +0900

    세팅

---
+++ src/main/java/jjfactory/psastudy/PsaStudyApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.psastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jjfactory/psastudy/PsaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.psastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
